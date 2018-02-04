a3aedc73534986519a6947e4393e4ce9732644f4
gabor <gabor.nemeth.de@gmail.com>
2018-02-04 20:54:42 +0100

    Changed controllers, cleanup code.

--- ssn1k/Synth.cpp
+++ ssn1k/Synth.cpp
@@ -29,12 +29,7 @@ void Synth::init(size_t voices) {
 	voiceCount_ = voices > 64 ? 64 : voices;
 	//overlayValue_ = .0f;
 	// create controls
-	controls_ = MALLOC(Ctrl*, SSN1K_CI_COUNT);
-	Ctrl* controls = MALLOC(Ctrl, SSN1K_CI_COUNT);
-	for (int i = 0; i < SSN1K_CI_COUNT; i++) {
-		controls_[i] = &controls[i];
-		controls[i].set(0.0, 0.0, 1.0f);
-	}
+	createControls(SSN1K_CI_COUNT);
 	//sweepCtrls = 0;
 	//paSweepCtrls = new Ctrl*[256];
 	// add voices
@@ -64,34 +59,14 @@ Synth::~Synth() {
 	FREE(controls_);
 }
 
-//int Synth::isActive() {
-//	int ret = this->env1.isActive();
-//	return ret;
-//}
-
-//void Synth::setCtrls(SynthCtrls* controls) {
-//	Mdl::setCtrls((Ctrl*)pCtrls);
-//	env1_.setCtrls((Ctrl*)&((SynthCtrls*)pCtrls)->env1);
-//	env2_.setCtrls((Ctrl*)&((SynthCtrls*)pCtrls)->env2);
-//	env3_.setCtrls((Ctrl*)&((SynthCtrls*)pCtrls)->env3);
-//	env4_.setCtrls((Ctrl*)&((SynthCtrls*)pCtrls)->env4);
-//	osc1_.setCtrls((Ctrl*)&((SynthCtrls*)pCtrls)->osc1);
-//	osc2_.setCtrls((Ctrl*)&((SynthCtrls*)pCtrls)->osc2);
-//	lfo1_.setCtrls((Ctrl*)&((SynthCtrls*)pCtrls)->lfo1);
-//	lfo2_.setCtrls((Ctrl*)&((SynthCtrls*)pCtrls)->lfo2);
-//	//flt_.setCtrls((Ctrl*)&((SynthCtrls*)pCtrls)->flt);
-//}
-
-void Synth::setup(CtrlSetting* data) {
-	while (data->id != -1) {
-		Ctrl* ctrl = getControl(data->id);
-		ctrl->set(data->value);
-		data++;
+float Synth::run() {
+	float smp = 0.0f;
+	for (size_t j = 0; j<voiceCount_; j++) {
+		if (voices_[j]->isActive()) {
+			smp += voices_[j]->run();
+		}
 	}
-}
-
-Ctrl* Synth::getControl(size_t id) {
-	return controls_[id];
+	return smp;
 }
 
 void Synth::run(void* buffer, size_t start, size_t end) {
@@ -102,105 +77,26 @@ void Synth::run(void* buffer, size_t start, size_t end) {
 				smp += voices_[j]->run();
 			}
 		}
-		short out = (short)floor(32767.0f * smp * controls_[SSN1K_CI_SynthAmp]->get().f);
+		short out;
+		smp *= controls_[SSN1K_CI_SynthAmp]->get().f;
+		if (smp < 1.0f) {
+			if (smp > -1.0f) {
+				out = (short)round(32767.0f * smp);
+			} else {
+				out = -32767;
+			}
+		} else {
+			out = 32767;
+		}
 		short left = (short)(controls_[SSN1K_CI_SynthBal]->get().f * out);
 		short right = (short)((1.0f - controls_[SSN1K_CI_SynthBal]->get().f) * out);
 		((short*)buffer)[2 * i] += left;
 		((short*)buffer)[2 * i + 1] += right;
 	}
 }
-void Synth::run(void* buffer, size_t ix) {
-	float smp = 0.0f;
-	for (size_t j = 0; j<voiceCount_; j++) {
-		if (voices_[j]->isActive()) {
-			smp += voices_[j]->run();
-		}
-	}
-	short out = (short)floor(32767.0f * smp * controls_[SSN1K_CI_SynthAmp]->get().f);
-	short left = (short)(controls_[SSN1K_CI_SynthBal]->get().f * smp);
-	short right = (short)((1.0f - controls_[SSN1K_CI_SynthBal]->get().f) * smp);
-	((short*)buffer)[2 * ix] += left;
-	((short*)buffer)[2 * ix + 1] += right;
-}
-
-//float Synth::run(float in) {
-//#ifdef _PROFILE
-//	profiler.start();
-//	profiler.enter(1);
-//#endif
-//	// update sweepCtrls
-//	//for (int i=0; i<iSweepCtrls; i++) {
-//	//	paSweepCtrls[i]->change();
-//	//}
-//#ifdef _PROFILE
-//	profiler.leave(1);
-//#endif
-//	float prevSmp = smp_;
-//
-//	//if (env1.isActive())
-//	{
-//		SynthCtrls* ctrls = (SynthCtrls*)this->ctrls_;
-//#ifdef _PROFILE
-//	profiler.enter(2);
-//#endif
-//		float lfo1 = lfo1.run(0.0f, 0, 0);
-//		float lfo2 = lfo2.run(0.0f, 0, 0);
-//
-//		ctrls->env1.dc.set(fLfo1, 0);
-//		ctrls->env2.dc.set(fLfo2, 0);
-//#ifdef _PROFILE
-//	profiler.leave(2);
-//#endif
-//
-//#ifdef _PROFILE
-//	profiler.enter(3);
-//#endif
-//		float fAM = env1.run(ctrls->amp.get().fValue);
-//		float fFM = 5000 * env2.run(0);
-//		float fPM = env3.run(0);
-//		float fCM = 2*env4.run(0);
-//#ifdef _PROFILE
-//	profiler.leave(3);
-//#endif
-//#ifdef _PROFILE
-//	profiler.enter(4);
-//#endif
-//		smp = osc1.run(fAM, fFM, fPM);
-//		smp = osc2.run(smp, fFM, fPM);
-//#ifdef _PROFILE
-//	profiler.leave(4);
-//#endif
-//#ifdef _PROFILE
-//	profiler.enter(5);
-//#endif
-//		smp = flt.run(smp, fCM);
-//#ifdef _PROFILE
-//	profiler.leave(5);
-//#endif
-//		//smp = env4.smp;
-//#ifdef _PROFILE
-//	profiler.enter(6);
-//#endif
-//		// Adding overlay
-//		if (env1.iOverlayCounter > 0)
-//		{
-//			if (env1.iOverlayCounter == OVERLAY_LENGTH-1)
-//			{
-//				fOverlayValue = fPrevSmp;
-//			}
-//			float fFact = smooth((float)env1.iOverlayCounter/OVERLAY_LENGTH);
-//			smp = fOverlayValue*fFact + (1.0f - fFact) * smp;
-//		}
-//	}
-//#ifdef _PROFILE
-//	profiler.leave(6);
-//	profiler.end();
-//#endif
-//	return smp;
-//}
-
 void Synth::setNote(int note, int duration, float velocity) {
 	// get free voice
+	//printf("synth: %llx, note: %d, velocity: %f, duration: %d\n", this, note, velocity, duration);
 	Voice* voice = voices_[nextVoice_];
 	nextVoice_ = (nextVoice_ + 1) % voiceCount_;
 	voice->setNote(note, duration, velocity);
--- ssn1k/Synth.h
+++ ssn1k/Synth.h
@@ -13,17 +13,19 @@
 NS_SSN1K_BEGIN
 
 struct SynthCtrls : public MdlCtrls {
+	Ctrl* tune;
 	EnvCtrls env1, env2, env3, env4;
 	OscCtrls osc1, osc2, lfo1, lfo2;
 	FltCtrls flt;
 };
 
 enum SynthControlIds {
-	 SSN1K_CI_SynthMix
+	 SSN1K_CI_SynthMix	// 0
 	,SSN1K_CI_SynthAmp
-	,SSN1K_CI_SynthBal		// = dc
+	,SSN1K_CI_SynthBal
+	,SSN1K_CI_Tune
 
-	,SSN1K_CI_Env1Mix
+	,SSN1K_CI_Env1Mix	// 3
 	,SSN1K_CI_Env1Amp
 	,SSN1K_CI_Env1DC
 	,SSN1K_CI_Env1Atk
@@ -31,7 +33,7 @@ enum SynthControlIds {
 	,SSN1K_CI_Env1Sus
 	,SSN1K_CI_Env1Rel
 
-	,SSN1K_CI_Env2Mix
+	,SSN1K_CI_Env2Mix	// 10
 	,SSN1K_CI_Env2Amp
 	,SSN1K_CI_Env2DC
 	,SSN1K_CI_Env2Atk
@@ -39,7 +41,7 @@ enum SynthControlIds {
 	,SSN1K_CI_Env2Sus
 	,SSN1K_CI_Env2Rel
 
-	,SSN1K_CI_Env3Mix
+	,SSN1K_CI_Env3Mix	// 17
 	,SSN1K_CI_Env3Amp
 	,SSN1K_CI_Env3DC
 	,SSN1K_CI_Env3Atk
@@ -47,7 +49,7 @@ enum SynthControlIds {
 	,SSN1K_CI_Env3Sus
 	,SSN1K_CI_Env3Rel
 
-	,SSN1K_CI_Env4Mix
+	,SSN1K_CI_Env4Mix	// 24
 	,SSN1K_CI_Env4Amp
 	,SSN1K_CI_Env4DC
 	,SSN1K_CI_Env4Atk
@@ -55,7 +57,7 @@ enum SynthControlIds {
 	,SSN1K_CI_Env4Sus
 	,SSN1K_CI_Env4Rel
 
-	,SSN1K_CI_Osc1Mix
+	,SSN1K_CI_Osc1Mix	// 31
 	,SSN1K_CI_Osc1Amp
 	,SSN1K_CI_Osc1DC
 	,SSN1K_CI_Osc1Note
@@ -64,7 +66,7 @@ enum SynthControlIds {
 	,SSN1K_CI_Osc1Psw
 	,SSN1K_CI_Osc1Wav
 
-	,SSN1K_CI_Osc2Mix
+	,SSN1K_CI_Osc2Mix	// 39
 	,SSN1K_CI_Osc2Amp
 	,SSN1K_CI_Osc2DC
 	,SSN1K_CI_Osc2Note
@@ -73,7 +75,7 @@ enum SynthControlIds {
 	,SSN1K_CI_Osc2Psw
 	,SSN1K_CI_Osc2Wav
 
-	,SSN1K_CI_Lfo1Mix
+	,SSN1K_CI_Lfo1Mix	// 47
 	,SSN1K_CI_Lfo1Amp
 	,SSN1K_CI_Lfo1DC
 	,SSN1K_CI_Lfo1Note
@@ -103,7 +105,6 @@ enum SynthControlIds {
 class Voice;
 //*****************************************************************************
 class Synth : public Mdl {
-protected: PROP_R(Ctrl**, controls);
 protected:
 	//Array sweepControls;	//Array<Ctrl>
 	//Ctrl **paSweepCtrls;
@@ -121,15 +122,15 @@ public:
 	_Profiler profiler;
 
 #endif
-	void setup(CtrlSetting* data);
+	//void setup(CtrlSetting* data);
 	//void setCtrls(SynthCtrls* controls);
-	Ctrl* getControl(size_t id);
+	//Ctrl* getControl(size_t id);
 	//int isActive();
 	//inline float getSmp() { return this->smp; }
 	//inline float getDelta() { return this->smp - this->prevSmp; }
-	//void run(float in);
+	float run();
 	void run(void* buffer, size_t start, size_t end);
-	void run(void* buffer, size_t ix);
+	//void run(void* buffer, size_t ix);
 	void setNote(int note, int duration, float velocity);
 	void setGate(float velocity);
 	//void setCtrlSweep(int id, float fStart, float fEnd, int iCycles);
