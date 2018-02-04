62eee465cacf2286a32abaa629e78c4d52e36292
gabor <gabor.nemeth.de@gmail.com>
2018-02-04 20:55:43 +0100

    Controller access changed, run method modified.

--- ssn1k/Voice.cpp
+++ ssn1k/Voice.cpp
@@ -6,19 +6,19 @@ NS_SSN1K_BEGIN
 Voice::Voice(Synth* synth, int id) {
 	synth_ = synth;
 	id_ = id;
-	SynthCtrls* controls = (SynthCtrls*)synth->controls();
-	env1_.setControls(&controls->env1);
-	env2_.setControls(&controls->env2);
-	env3_.setControls(&controls->env3);
-	env4_.setControls(&controls->env4);
-	osc1_.setControls(&controls->osc1);
-	osc2_.setControls(&controls->osc2);
-	size_t ix = offsetof(OscCtrls, note);
-	osc1_.note(&this->note_);
-	osc2_.note(&this->note_);
-	lfo1_.setControls(&controls->lfo1);
-	lfo2_.setControls(&controls->lfo2);
-	flt_.setControls(&controls->flt);
+	SynthCtrls* ctrls = (SynthCtrls*)synth->controls_;
+	env1_.controls_ = (Ctrl**)&ctrls->env1;
+	env2_.controls_ = (Ctrl**)&ctrls->env2;
+	env3_.controls_ = (Ctrl**)&ctrls->env3;
+	env4_.controls_ = (Ctrl**)&ctrls->env4;
+	osc1_.controls_ = (Ctrl**)&ctrls->osc1;
+	osc2_.controls_ = (Ctrl**)&ctrls->osc2;
+	//size_t ix = offsetof(OscCtrls, note);
+	//((OscCtrls*)osc1_.controls_)->note = &note_;
+	//((OscCtrls*)osc2_.controls_)->note = &note_;
+	lfo1_.controls_ = (Ctrl**)&ctrls->lfo1;
+	lfo2_.controls_ = (Ctrl**)&ctrls->lfo2;
+	flt_.controls_ = (Ctrl**)&ctrls->flt;
 	duration_ = 0;
 }
 
@@ -27,8 +27,8 @@ Voice::~Voice() {
 
 void Voice::setNote(int note, int duration, float velocity) {
 	//printf("voice: %d, note: %d, velocity: %f, duration: %d\n", id_, note, velocity, duration);
-	SynthCtrls* controls = (SynthCtrls*)synth_->controls();
-	note_.set((float)note, 0);
+	SynthCtrls* controls = (SynthCtrls*)synth_->controls_;
+	note_.set((float)note, false);
 	duration_ = duration;
 
 	// key on
@@ -36,19 +36,22 @@ void Voice::setNote(int note, int duration, float velocity) {
 	env1_.setGate(velocity);
 	env2_.setGate(velocity);
 	env3_.setGate(velocity);
-	//env4_.setGate(velocity);
+	env4_.setGate(velocity);
 }
 float Voice::run() {
 	// run LFOs
-	float lfo1 = lfo1_.run(0.0f, 0.0f, 0.0f);	//1.0 - this.lfos[0].amp + (this.lfos[0].amp + this.lfos[0].run(1.0, 0.0, 0.0))/2;
-	float lfo2 = lfo2_.run(0.0f, 0.0f, 0.0f);
+	float lfo1 = lfo1_.run(1.0f, 0.0f, 0.0f, 0.0f);	//1.0 - this.lfos[0].amp + (this.lfos[0].amp + this.lfos[0].run(1.0, 0.0, 0.0))/2;
+	float lfo2 = lfo2_.run(1.0f, 0.0f, 0.0f, 0.0f);
 	// run main oscillators
-	float amp = env1_.run(1.0f);
-	float psw = env2_.run(1.0f) + lfo2;
+	float am = env1_.run(1.0f);
+	float pm = env2_.run(1.0f) + lfo2;
 	float cut = env3_.run(1.0f);
-	float smp1 = osc1_.run(amp, lfo1, psw);
-	float smp2 = osc2_.run(amp, lfo1, psw);
-	float out = flt_.run(smp1 + smp2, cut);
+	float fm = env4_.run(lfo1);
+	float tune = note_.get().f + synth_->getControl(SSN1K_CI_Tune)->get().f;
+	float smp1 = osc1_.run(1.0f, tune, fm, pm);
+	float smp2 = osc2_.run(smp1, tune, fm, pm);
+	float out = flt_.run(am * smp2, cut);
+	//float out = smp1 + smp2;
 	if (duration_ > 0) {
 		duration_--;
 	} else {
@@ -58,7 +61,7 @@ float Voice::run() {
 			env1_.setGate(0.0f);
 			env2_.setGate(0.0f);
 			env3_.setGate(0.0f);
-			//env4_.setGate(0.0f);
+			env4_.setGate(0.0f);
 			duration_ = -1;
 		}
 	}
